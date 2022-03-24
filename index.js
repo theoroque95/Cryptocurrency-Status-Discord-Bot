41ad4dfd361ab07a60aa6397c74d1b7c1e0da4fa
Theo Roque <theoroque95@gmail.com>
2022-03-25 00:12:09 +0800

    Move Peso Sign To Front

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function getPrices() {
 				}
 			})
 
-			client.guilds.find(guild => guild.id === process.env.SERVER_ID).me.setNickname(`${(currentPrice).toLocaleString().replace(/,/g,process.env.THOUSAND_SEPARATOR)}${process.env.CURRENCY_SYMBOL}`)
+			client.guilds.find(guild => guild.id === process.env.SERVER_ID).me.setNickname(`${process.env.CURRENCY_SYMBOL} ${(currentPrice).toLocaleString().replace(/,/g,process.env.THOUSAND_SEPARATOR)}`)
 
 			console.log('Updated price to', currentPrice)
 		}
